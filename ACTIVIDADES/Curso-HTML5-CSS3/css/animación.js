76dfd31eb5d0b9fc82f2768ef90142ba3b55ddff
ErosMZ <erosmunozzanon@gmail.com>
2024-12-19 16:11:23 +0100

    eproyecto

--- ACTIVIDADES/Curso-HTML5-CSS3/css/animación.js
+++ ACTIVIDADES/Curso-HTML5-CSS3/css/animación.js
@@ -26,3 +26,19 @@ window.addEventListener("load", () => {
     }, { once: true });
 });
 
+var enlaceMenu;
+
+function iniciarMenu() 
+{
+	enlaceMenu = document.querySelector("#page>nav>a");
+	enlaceMenu.addEventListener("click", despliegaMenu, false);
+}
+
+function despliegaMenu()
+{
+	document.querySelector("#page>nav>ul").classList.toggle('desplegado');
+}
+
+window.addEventListener("load", iniciarMenu, false);
+
+
